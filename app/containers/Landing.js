6206729bae5497db7169b230f0bbb88bc23dd55b
Eddie Wang <eddie@eddiewang.me>
2017-03-06 16:56:15 -0500

    latest

--- app/containers/Landing.js
+++ app/containers/Landing.js
@@ -5,116 +5,44 @@ import Webcam from 'components/Webcam'
 import { postRequest } from 'utils/fetch'
 import { LineChart, Line, BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts'
 import ReactHighcharts from 'react-highcharts'
-const emotionData = { "Angry": 0.1240934516, "Sad": 0.391903375, "Neutral": 0.1427304191, "Surprise": 0.1225001438, "Fear": 0.1069961407, "Happy": 0.1117764698 }
 
-
-const options = {
-    chart: {
-        type: 'area'
-        }
-    ,
-    title: {
-        text: 'Emotional Impact of Ad Based on Current Facial State'
-    },
-    subtitle: {
-        text: 'Source: Indico API'
-    },
-    xAxis: {
-        type: 'datetime',
-        tickPixelInterval: 1000
-    },
-    yAxis: {
-        title: {
-            text: 'Percent'
-        }
-    },
-    tooltip: {
-        formatter: function () {
-                        return '<b>' + this.series.name + '</b><br/>' +
-                            ReactHighcharts.Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                            ReactHighcharts.Highcharts.numberFormat(this.y, 2);
-                    }
-    },
-    plotOptions: {
-        area: {
-            stacking: 'percent',
-            lineColor: '#ffffff',
-            lineWidth: 1,
-            marker: {
-                lineWidth: 1,
-                lineColor: '#ffffff'
-            }
-        }
-    },
-    series: [{
-        name: 'Angry',
-        data: []
-    }, {
-        name: 'Sad',
-        data: []
-    }, {
-        name: 'Neutral',
-        data: []
-    }, {
-        name: 'Fear',
-        data: []
-    }, {
-        name: 'Surprise',
-        data: []
-    }, {
-        name: 'Happy',
-        data: []
-    }
-    ]
-}
 
 class Landing extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      screenshot: null,
-      emotions: [],
-      start: false
-    }
   }
 
   componentDidMount() {
-    setInterval(() => {
-      if(this.state.start) {
-        const image = {
-          image: this.refs.webcam.getScreenshot()
-        }
-        const fetchEmotion = postRequest('/emotion', image, (err) => console.log(err))
-        .then((result) => {
-          let chart = this.refs.chart.getChart();
-          let [Angry, Sad, Neutral, Fear, Surprise, Happy] = chart.series
-          console.log(chart.series)
-          const date = (new Date()).getTime()
-          const emotions = result.data.result
-          console.log(date, emotions)
-          Angry.addPoint({x: date, y: emotions.Angry})
-          Sad.addPoint({x: date, y: emotions.Sad})
-          Neutral.addPoint({x: date, y:emotions.Neutral})
-          Surprise.addPoint({x: date, y: emotions.Surprise})
-          Fear.addPoint({x: date, y: emotions.Fear})
-          Happy.addPoint({x: date, y: emotions.Happy})
-        })
-      }
-    }, 5000)
-  }
-
-  capture = () => {
-    this.setState({start: !this.state.start})
+    // setInterval(() => {
+    //   if(this.state.start) {
+    //     const image = {
+    //       image: this.refs.webcam.getScreenshot()
+    //     }
+    //     const fetchEmotion = postRequest('/emotion', image, (err) => console.log(err))
+    //     .then((result) => {
+    //       let chart = this.refs.chart.getChart();
+    //       let [Angry, Sad, Neutral, Fear, Surprise, Happy] = chart.series
+    //       console.log(chart.series)
+    //       const date = (new Date()).getTime()
+    //       const emotions = result.data.result
+    //       console.log(date, emotions)
+    //       Angry.addPoint({x: date, y: emotions.Angry})
+    //       Sad.addPoint({x: date, y: emotions.Sad})
+    //       Neutral.addPoint({x: date, y:emotions.Neutral})
+    //       Surprise.addPoint({x: date, y: emotions.Surprise})
+    //       Fear.addPoint({x: date, y: emotions.Fear})
+    //       Happy.addPoint({x: date, y: emotions.Happy})
+    //     })
+    //   }
+    // }, 5000)
   }
 
   render () {
-    console.log(this.state.emotions)
     return (
       <div className='main-container'>
         <Title>hello universe.</Title>
         <Webcam screenshotFormat="image/png" height={300} width={300} audio={false} ref="webcam"/>
         <button onClick={this.capture}>Capture</button>
-        <ReactHighcharts config={options} ref="chart" />
       </div>
     )
   }
--- server/api/api.js
+++ server/api/api.js
@@ -3,25 +3,38 @@ const indico = require('indico.io')
 const R = require('ramda')
 const processEmotion = require('../utils')
 const config = require('./config')
+const axios = require('axios')
+
+const microsoft = axios.create({
+  baseURL: 'https://westus.api.cognitive.microsoft.com/',
+  timeout: 20000,
+  'headers': {
+    'Ocp-Apim-Subscription-Key': '7776acba38ff4610a4c0b1cec81a0299',
+    'Content-Type': 'application/octet-stream'
+  }
+});
+
 indico.apiKey =  config.key;
 
 router.post('/test', (req, res) => {
   res.send({result: 'success'})
 })
 
-router.post('/emotion', (req, res) => {
+router.post('/microsoft', (req, res) => {
   const image = req.body.image.replace(/^data:image\/(png|jpg);base64,/, "")
-  indico.fer(image)
-    .then((result) => {
-      const emotions = Object.assign(result, {time: new Date()})
-      console.log(emotions)
+
+  const buffer = Buffer.from(image, 'base64')
+  microsoft.post('/emotion/v1.0/recognize', buffer)
+    .then(function (response) {
+      console.log(response.data);
       res.status(200).json({
-        result: emotions
+        result: response.data
       })
     })
-    .catch((err) => {
-      console.log(err)
+    .catch(function (error) {
+      console.log(error);
     });
+
 })
 
 module.exports = router;
